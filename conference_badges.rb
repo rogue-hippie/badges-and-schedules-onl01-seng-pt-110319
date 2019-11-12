e11c2b22c743d3c3cc963aef493dcc7e9166fac8
Whitney Hodgkins <hodgkins.whitney@gmail.com>
2019-11-12 21:43:30 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,32 @@
-# Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+  
+  
+def batch_badge_creator(attendees)
+  badges =[]
+  
+  attendees.each do |name|
+    badges << badge_maker(name)
+  end
+  badges
+end
+
+
+def assign_rooms(attendees)
+  room_assignments = []
+  
+  number = 1 
+  
+  attendees.each do |name|
+    room_assignments << "Hello, #{name}! You'll be assigned to room #{number}!"
+    number = number +1 
+  end
+  room_assignments
+end
+
+def printer(attendees)
+  batch_badge_creator(attendees).each do |badge|
+    puts badge
+  end
+end
